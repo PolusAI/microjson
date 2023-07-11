965c52982adf018c0fd22508289fd961ecbddd8d
Bengt Ljungquist <bengt@ljungquist.info>
2023-07-11 13:11:11 -0400

    Change module exposure

--- microjson/__init__.py
+++ microjson/__init__.py
@@ -1,4 +1,4 @@
 from .model import MicroJSON, GeoJSON
-from .automodel import GeoJSONAuto, MicroJSONAuto
-from .roundtrip import MicroJSON as MicroJSONRound
+#from .automodel import GeoJSONAuto, MicroJSONAuto
+#from .roundtrip import MicroJSON as MicroJSONRound
 from .utils import gather_example_files
