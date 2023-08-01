a9d064c66825ab0cfe4ea03012fe90816ab8c3f0
Bengt Ljungquist <bengt@ljungquist.info>
2023-08-01 03:01:19 -0400

    Updated structure

--- src/microjson/__init__.py
+++ src/microjson/__init__.py
@@ -2,3 +2,5 @@ from .model import MicroJSON, GeoJSON
 #from .automodel import GeoJSONAuto, MicroJSONAuto
 #from .roundtrip import MicroJSON as MicroJSONRound
 from .utils import gather_example_files
+
+__version__ = "0.1.1"
