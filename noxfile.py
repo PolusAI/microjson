822693c2d6446d69a6c0406b2be4bf6b061b5caa
Bengt Ljungquist <bengt@ljungquist.info>
2023-10-13 12:33:52 -0400

    Added multilevel metadata test

--- noxfile.py
+++ noxfile.py
@@ -5,44 +5,54 @@ import json
 
 @nox.session(python="3.9")
 def generate_geojson_pydantic(session):
-    session.install("-r", "requirements-dev.txt")
+    session.install("-r", "requirements-nox.txt")
     session.install("-e", ".")
-    session.run("datamodel-codegen",
-                "--reuse-model",
-                "--snake-case-field",
-                "--input", "external",
-                "--output", "geojson",
-                "--target", "3.9")
+    session.run(
+        "datamodel-codegen",
+        "--reuse-model",
+        "--snake-case-field",
+        "--input",
+        "external",
+        "--output",
+        "geojson",
+        "--target",
+        "3.9",
+    )
 
 
 @nox.session(python="3.9")
 def generate_roundtrip_json_schema(session):
-    session.install("-r", "requirements-dev.txt")
+    session.install("-r", "requirements-nox.txt")
     session.install("-e", ".")
     # generate GeoJSON schema
     # Get the schema for the model as a dictionary
     schema_dict = GeoJSON.model_json_schema()
     # Add 'null' to the list of allowed types for the 'geometry' field of
     # Feature object
-    schema_dict = schema_dict['$defs']['Feature']
-    schema_dict['properties']['geometry']['anyOf'].append({'type': 'null'})
-    with open('geojson_schema.json', 'w') as f:
+    schema_dict = schema_dict["$defs"]["Feature"]
+    schema_dict["properties"]["geometry"]["anyOf"].append({"type": "null"})
+    with open("geojson_schema.json", "w") as f:
         f.write(json.dumps(GeoJSON.model_json_schema(), indent=4))
 
-    with open('microjson_schema.json', 'w') as f:
+    with open("microjson_schema.json", "w") as f:
         f.write(json.dumps(MicroJSON.model_json_schema(), indent=4))
 
 
 @nox.session(python="3.9")
 def generate_roundtrip_pydantic(session):
     # generate pydantic model from the GeoJSON schema
-    session.install("-r", "requirements-dev.txt")
-    session.run("datamodel-codegen",
-                "--reuse-model",
-                "--snake-case-field",
-                "--input", "geojson_schema.json",
-                "--output", "geojson/roundtrip.py",
-                "--target", "3.9")
+    session.install("-r", "requirements-nox.txt")
+    session.run(
+        "datamodel-codegen",
+        "--reuse-model",
+        "--snake-case-field",
+        "--input",
+        "geojson_schema.json",
+        "--output",
+        "geojson/roundtrip.py",
+        "--target",
+        "3.9",
+    )
 
 
 @nox.session
@@ -51,9 +61,11 @@ def typescript(session):
     session.run("npm", "install", "json-schema-to-typescript", external=True)
 
     # Use json-schema-to-typescript
-    session.run("npx",
-                "json-schema-to-typescript",
-                "microjson_schema.json",
-                "-o",
-                "microjson.d.ts",
-                external=True)
+    session.run(
+        "npx",
+        "json-schema-to-typescript",
+        "microjson_schema.json",
+        "-o",
+        "microjson.d.ts",
+        external=True,
+    )
