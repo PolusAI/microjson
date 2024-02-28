b38d648c0676b42c42ae8de888a926d4a46cdf88
Bengt Ljungquist <bengt@ljungquist.info>
2024-02-28 14:52:42 -0500

    Bump version 0.1.9 -> 0.1.10

--- src/microjson/__init__.py
+++ src/microjson/__init__.py
@@ -1,5 +1,6 @@
 from .model import MicroJSON, GeoJSON
-from .utils import gather_example_files,OmeMicrojsonModel, MicrojsonBinaryModel
+from .utils import gather_example_files
+from .utils import OmeMicrojsonModel
+from .utils import MicrojsonBinaryModel
 
-__version__ = "0.1.9"
-
+__version__ = "0.1.10"
