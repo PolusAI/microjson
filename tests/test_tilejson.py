f7ce6138c52a6c1b3146a190c5fdf52276f44e23
Bengt Ljungquist <bengt@ljungquist.info>
2024-08-05 13:39:34 -0400

    created default package with less dependencies (#13)

--- tests/test_tilejson.py
+++ tests/test_tilejson.py
@@ -2,7 +2,7 @@ import json
 import pytest
 from pydantic import ValidationError
 from microjson.tile import TileJSON
-from microjson.utils import gather_example_files
+from microjson.fileutils import gather_example_files
 
 
 # Define the directories containing the example JSON files
