3af84228c999b4c5c041050c269c936404a9b8b0
Bengt Ljungquist <bengt@ljungquist.info>
2024-08-14 15:19:37 -0400

    Fixed test issues

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,13 +6,14 @@ import shutil
 import tempfile
 from pathlib import Path
 from typing import Union
-import numpy as np
 import pytest
-import skimage as sk
+
 HAS_BFIO = False
 try:
     import bfio
     HAS_BFIO = True
+    import numpy as np
+    import skimage as sk
 except ImportError:
     pass
 
