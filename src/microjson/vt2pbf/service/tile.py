2dbb2522466ede858790d6fcc23c25b6141fe5df
Bengt Ljungquist <bengt@ljungquist.info>
2025-01-13 05:13:29 -0500

    Added reader example

--- src/microjson/vt2pbf/service/tile.py
+++ src/microjson/vt2pbf/service/tile.py
@@ -1,34 +1,25 @@
-from typing import List
-
-from google import protobuf
+from typing import List, Optional
 
 from microjson.vt2pbf.config import EXTEND
 from microjson.vt2pbf.exceptions import LayerExistError
 from microjson.vt2pbf.service.layer import Layer
 
-from microjson.vt2pbf.mapbox.protobuf_3 import vector_tile_pb2
+from mapbox_vector_tile.Mapbox import vector_tile_pb2
 
-# TODO: Implement support for protobuf version >= 4
-"""
-if int(protobuf.__version__.split('.')[0]) < 4:
-    from microjson.vt2pbf.mapbox.protobuf_3 import vector_tile_pb2
-else:
-    from mapbox_vector_tile.Mapbox import vector_tile_pb2
-#    from microjson.vt2pbf.mapbox.protobuf_4 import vector_tile_pb2
-"""
 
 class Tile:
-    def __init__(self, extend: int = None):
-        self.tile_pbf = vector_tile_pb2.Tile()
+    def __init__(self, extend: Optional[int] = None):
+        self.tile_pbf = vector_tile_pb2.tile()
         self.extend = extend or EXTEND
-        self._layers = {}
+        self._layers: dict[str, Layer] = {}
 
     def serialize_to_bytestring(self) -> bytes:
         return self.tile_pbf.SerializeToString()
 
     def add_layer(self, name: str, features: List[dict]):
         if name in self._layers:
-            raise LayerExistError('Layer with this name already exist in the tile')
+            raise LayerExistError(
+                'Layer with this name already exist in the tile')
 
         layer = Layer(self.tile_pbf, name, extend=self.extend)
         for feature in features:
